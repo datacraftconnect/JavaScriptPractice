506b4c7cdb09ef2392a82cdf2222082bde46b903
datacraftconnect <datacraftconnect@gmail.com>
2025-04-15 23:18:19 +0100

    AddingPractice3and4

---
+++ 3PrintingStatement/index.js
@@ -0,0 +1,17 @@
+let newAge;
+newAge = 80;
+
+//Printing Statement
+//alert( newAge )
+
+document.write( newAge ) //render value in UI
+document.writeln( newAge )
+
+//confirm( newAge ) //similar to alert but has ok & cancel button
+
+//let userName = prompt ("Enter your name: ")
+
+//document.writeln( userName )
+console.error( )
+console.warn( )
+console.clear( )
---
+++ 4Datatypes/index.js
@@ -0,0 +1,30 @@
+//Primitive Datatypes
+//number
+var num = 120;
+var num = 1.25
+console.log( num )
+//String
+let userName = "My Name is Karthik"
+console.log (userName)
+//Boolena
+let condition = true;
+console.log( condition )
+//Undefined
+let noValue = undefined;
+//or
+let noValue1; // treats as undefined
+//Null
+let emptyVaue = null;
+
+//Non Premetive Data Types - storing multiple values
+//Array
+let multipleUsers = ["Faceb;ook","Insta","Orkut"]
+console.log(multipleUsers);
+console.log(multipleUsers[0], multipleUsers[1]);
+console.log(multipleUsers.length);
+console.log(multipleUsers.length-1);
+console.log(multipleUsers[multipleUsers.length-1]);
+//object - behaviour & Property
+let vechicle = { Type: "FourWheeler", Brand: "Hyundai", Price: 5000, FuelType: "Petrol"}
+console.log( vechicle )
+console.log(vechicle.Brand)
