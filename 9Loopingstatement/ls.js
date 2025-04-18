ad7c8fe2b51d72e2203b1552a34f7826c4428dbe
datacraftconnect <datacraftconnect@gmail.com>
2025-04-18 17:57:40 +0100

    practice9

---
+++ 9Loopingstatement/ls.js
@@ -0,0 +1,25 @@
+//looping Statement
+//for loop
+for (let i = 0; i<=10; i++) {
+//    if( i%2 == 0 ) console.log( i );
+    if( i%2 != 0 ) console.log( i );
+}
+
+//while loop
+let val = 10;
+console.log("printing While Loop")
+while(val>=1){
+    console.log(val);
+    val--;
+}
+
+//do while
+let num = 21;
+do{
+    console.log(num);
+    num++;
+}
+while(num<=20);
+
+//for, while with break & continue
+
